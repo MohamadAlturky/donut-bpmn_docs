3f273a16988920f9a1e17221431f48ac86195321
mohamad.alturky <m799678@gmail.com>
2024-06-23 02:59:59 +0300

    edit hero page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,12 +15,12 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Understand Our Tool In No Time ⏱️
           </Link>
         </div>
       </div>

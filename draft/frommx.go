8b1ec4b76eaee7a3beec1ae719f9c5cdc1cc114b
mohamad.alturky <m799678@gmail.com>
2024-06-23 03:12:31 +0300

    fixing a huge bug

---
+++ draft/frommx.go
@@ -0,0 +1,9 @@
+```md
+Let's see how to [Create a page](/create-a-page).
+```
+
+```md
+Let's see how to [Create a page](./create-a-page.md).
+```
+
+**Result:** Let's see how to [Create a page](./create-a-page.md).

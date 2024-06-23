b8f480e29173ae8242a7b1035c41b318b373f47e
mohamad.alturky <m799678@gmail.com>
2024-06-23 03:34:52 +0300

    editing landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Understand Our Tool In No Time ⏱️
+            Understand How We Built Our Tool 🛠
           </Link>
         </div>
       </div>
